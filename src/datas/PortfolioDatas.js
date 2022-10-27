f4c777cc6a4b0eb3e215f02bdb420f5571f431ba
Marc LANTOL <marc.lantol@gmail.com>
2022-10-27 15:19:08 +0200

    Add some new projects

--- src/datas/PortfolioDatas.js
+++ src/datas/PortfolioDatas.js
@@ -1,18 +1,20 @@
 // Importer les images ici
 import post from "../assets/projects/post.png";
 import movieflix from "../assets/projects/movieflix.png";
+import obsstjean from "../assets/projects/obsstjean.png";
+import lumios from "../assets/projects/lumios.png";
 
 const portfolioDatas = [
-  //   {
-  //     image: lumios,
-  //     lien: "https:lumios.com",
-  //     description:
-  //       "Aboutissement de ma formation, le projet professionel Lumios avait pour but d'apporter une solution à un porteur de projet",
-  //     github: "https:github.com/moumax/",
-  //     techno1: "React.js",
-  //     techno2: "d3js",
-  //     techno3: "Sass",
-  //   },
+  {
+    image: lumios,
+    lien: "https:lumios.com",
+    description:
+      "Aboutissement de ma formation, le projet professionel Lumios avait pour but d'apporter une solution à un porteur de projet",
+    github: "https:github.com/moumax/",
+    techno1: "React.js",
+    techno2: "d3js",
+    techno3: "Sass",
+  },
   {
     image: post,
     lien: "https:lumios.com",
@@ -30,6 +32,16 @@ const portfolioDatas = [
     github: "https:github.com/moumax/",
     techno1: "Html",
     techno2: "Css",
+  },
+  {
+    image: obsstjean,
+    lien: "https://obsstjean.fr/",
+    description:
+      "Le site de l'observatoire astronomique de Saint Jean le Blanc a été conçu par mes soins il y'a 3 ans. C'était mes premiers pas en développement web !",
+    github: "https:github.com/moumax/",
+    techno1: "Html",
+    techno2: "Css",
+    techno3: "Javascript",
   },
 ];
 
