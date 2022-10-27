56668150ce3405165e7e1d7be41bbd5f2c863dc8
Marc LANTOL <marc.lantol@gmail.com>
2022-10-27 15:22:32 +0200

    Fix background who was not covering the all page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { Projects } from "./pages/Projects/Projects";
 
 function App() {
   return (
-    <div className="h-[100vh] bg-slate-400">
+    //min-h-screen to have a full background cover
+    <div className="min-h-screen bg-slate-400">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
