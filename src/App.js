49b1b7dea4bab5d40f884ef840792a394ac04f47
Marc LANTOL <marc.lantol@gmail.com>
2022-11-03 23:38:33 +0100

    Create context for dark mode

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 import { Home } from "./pages/Home/Home";
 import { About } from "./pages/About/About";
 import { Contact } from "./pages/Contact/Contact";
 import { Projects } from "./pages/Projects/Projects";
+import { DarkContext } from "./context/DarkContext";
 
 function App() {
+  const [dark, setDark] = useState(false);
+
   return (
-    //min-h-screen to have a full background cover
-    <div className="min-h-screen bg-gradient-to-r from-blue-400 to-blue-900 p-10">
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </div>
+    <DarkContext.Provider value={[dark, setDark]}>
+      {/* min-h-screen to have a full background cover */}
+      <div className="min-h-screen bg-gradient-to-r from-blue-400 to-blue-900 p-10">
+        {!dark ? <div>toto</div> : <div>Pastoto</div>}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </DarkContext.Provider>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,29 @@
+import { useContext } from "react";
+import { DarkContext } from "../context/DarkContext";
+
 const Button = (props) => {
-  return (
-    <button
-      className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-      type={props.type || "button"}
-      onClick={props.onClick}
-    >
-      {props.children}
-    </button>
-  );
+  const [dark, setDark] = useContext(DarkContext);
+
+  if (dark === true) {
+    return (
+      <button
+        className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        type={props.type || "button"}
+        onClick={props.onClick}
+      >
+        {props.children}
+      </button>
+    );
+  } else
+    return (
+      <button
+        className="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        type={props.type || "button"}
+        onClick={props.onClick}
+      >
+        {props.children}
+      </button>
+    );
 };
 
 export default Button;
