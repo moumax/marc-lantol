3e5ae89fa0979a7b7c77fc1a8c3caf8488e0e508
Marc LANTOL <marc.lantol@gmail.com>
2022-11-04 18:45:11 +0100

    Fix home button style in dark mode

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,10 @@ const Button = (props) => {
   if (dark === true) {
     return (
       <button
-        className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        className={
+          props.className ||
+          "inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        }
         type={props.type || "button"}
         onClick={props.onClick}
       >
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,25 +7,36 @@ export function Home() {
   const navigate = useNavigate();
   const [french] = useContext(LangContext);
 
+  const buttonHome =
+    "inline-block px-14 py-8 bg-blue-600 text-white font-medium text-l leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out";
+
   return (
-    <div className="text-center ">
+    <div className="text-center">
       {french ? (
         <div className="mt-60">
           <h1 className="text-6xl mb-14 text-black">Bonjour !</h1>
-          <p className="text-4xl mb-14 text-black">
+          <p className="text-4xl mb-20 text-black">
             Je m'appelle Marc et je suis Développeur web Full Stack...
           </p>
-          <Button type={"button"} onClick={() => navigate("/about")}>
+          <Button
+            className={buttonHome}
+            type={"button"}
+            onClick={() => navigate("/about")}
+          >
             En savoir plus à mon sujet?
           </Button>
         </div>
       ) : (
         <div className="mt-60">
           <h1 className="text-6xl mb-14 text-black">Hello !</h1>
-          <p className="text-4xl mb-14 text-black">
+          <p className="text-4xl mb-20 text-black">
             My name is Marc and I'am a Full Stack web developer...
           </p>
-          <Button type={"button"} onClick={() => navigate("/about")}>
+          <Button
+            className={buttonHome}
+            type={"button"}
+            onClick={() => navigate("/about")}
+          >
             Want to learn more about me?
           </Button>
         </div>
